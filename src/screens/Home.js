1a65d57c5fdbd32be57815c55163139ae8557688
Sadirul Islam <sadirul.islam786@gmail.com>
2023-05-21 08:42:09 +0530

    context api merge fix

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,12 @@
-import { TouchableOpacity,Text, View, FlatList, SafeAreaView } from 'react-native'
+import { TouchableOpacity, Text, View, SafeAreaView } from 'react-native'
 import styleSheet from '../styles/Stylesheet'
 import { useContext } from 'react'
 import UserContext from '../context/Context';
 
-const Home = ({ navigation }) => {
 
-  const myContext = useContext(UserContext)
 
 const Home = ({navigation}) => {
+  const myContext = useContext(UserContext)
   const games = require('../json/GamesList.json')
   return (
     <View style={styleSheet.mainArea}>
@@ -30,25 +29,6 @@ const Home = ({navigation}) => {
         />
 
     </View>
-
-  return (
-    <SafeAreaView style={myContext}>
-      <View style={styleSheet.mainArea}>
-        <Text style={styleSheet.textStyle}>Excercises.</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Game')}>
-          <Text style={styleSheet.subtitleTextStyle}>
-            1. Find Larger Number
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styleSheet.subtitleTextStyle}>
-            2. Select Color
-          </Text>
-        </TouchableOpacity>
-
-      </View>
-    </SafeAreaView>
   )
 }
 
