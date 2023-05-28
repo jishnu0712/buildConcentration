7b7eba0974d39e7808422057975b647d0e725755
Sadirul Islam <sadirul.islam786@gmail.com>
2023-05-28 11:05:07 +0530

    Little error was there solved now

--- src/Helper/Helper.js
+++ src/Helper/Helper.js
@@ -6,10 +6,10 @@ export const randomNumber = (min, max) => {
 
 export const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    [array[i], array[j]] = [array[j], array[i]]
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  return array
+  return array;
 }
 
 
